8eba0226b65163b742f7b049b1de6f7f232ab7eb
Vyacheslav V Sokolov <deathless@t-sk.ru>
2020-04-30 17:22:46 +0700

    Update Program.cs

--- NAudioBPM/NAudioBPM/Program.cs
+++ NAudioBPM/NAudioBPM/Program.cs
@@ -22,6 +22,7 @@ namespace NAudioBPM
 
             if (bpmDetector.Groups.Length > 0)
             {
+                Console.WriteLine(String.Format("Most probable BPM is {0} ({1} samples)", bpmDetector.Groups[i].Tempo, bpmDetector.Groups[i].Count));
                 if (bpmDetector.Groups.Length > 1)
                 {
                     Console.WriteLine("Other options are:");
